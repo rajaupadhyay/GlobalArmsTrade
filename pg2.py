aa7f97824c2f93e761de66551b6fb820cac64d19
rajaupadhyay <upadhyayaraja@gmail.com>
2018-12-05 00:00:14 +0000

    new updates

---
+++ pg2.py
@@ -0,0 +1,138 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib import style
+style.use('ggplot')
+import networkx as nx
+
+zero_ds = pd.read_csv('edges/2000_edges.csv', sep=',', encoding='iso-8859-1')
+ten_ds = pd.read_csv('edges/2010_edges.csv', sep=',', encoding='iso-8859-1')
+seventeen_ds = pd.read_csv('edges/2017_edges.csv', sep=',', encoding='iso-8859-1')
+
+
+def createData(dataset):
+    targetNodes = list(dataset['Target'].values)
+    sourceNodes = list(dataset['Source'].values)
+    dictInDegree = {}
+    dictOutDegree = {}
+
+    for tn in targetNodes:
+        temprow = dataset.loc[dataset['Target'] == tn]
+        inEdgesCount = len(set(temprow['Source'].values))
+        dictInDegree[tn] = inEdgesCount
+
+    for sn in sourceNodes:
+        temprow = dataset.loc[dataset['Source'] == sn]
+        outEdgesCount = len(set(temprow['Target'].values))
+        dictOutDegree[sn] = outEdgesCount
+
+    in_values = sorted(set(dictInDegree.values()))
+    in_hist = [dictInDegree.values().count(x) for x in in_values]
+
+    out_values = sorted(set(dictOutDegree.values()))
+    out_hist = [dictOutDegree.values().count(x) for x in out_values]
+
+    return in_values, in_hist, out_values, out_hist
+
+
+# zero_in_values, zero_in_hist, zero_out_values, zero_out_hist = createData(zero_ds)
+# ten_in_values, ten_in_hist, ten_out_values, ten_out_hist = createData(ten_ds)
+# seventeen_in_values, seventeen_in_hist, seventeen_out_values, seventeen_out_hist = createData(seventeen_ds)
+
+'''
+Plot in-degree distribution
+'''
+# plt.figure()
+# plt.plot(zero_in_values,zero_in_hist,'ro-') # in-degree 2000
+# plt.plot(ten_in_values,ten_in_hist,'bx-') # in-degree 2010
+# plt.plot(seventeen_in_values,seventeen_in_hist,'gv-') # in-degree 2017
+# plt.legend(['2000', '2010', '2017'])
+# plt.xlabel('Degree')
+# plt.ylabel('Number of nodes')
+# plt.title('In-Degree distribution')
+# plt.tight_layout()
+# plt.show()
+
+'''
+Plot out-degree distribution
+'''
+# plt.figure()
+# plt.plot(zero_out_values,zero_out_hist,'ro-') # out-degree 2000
+# plt.plot(ten_out_values,ten_out_hist,'bx-') # out-degree 2010
+# plt.plot(seventeen_out_values,seventeen_out_hist,'gv-') # out-degree 2017
+# plt.legend(['2000', '2010', '2017'])
+# plt.xlabel('Degree')
+# plt.ylabel('Number of nodes')
+# plt.title('Out-Degree distribution')
+# plt.tight_layout()
+# plt.show()
+
+
+'''
+###########################
+###########################
+Create graphs
+###########################
+###########################
+'''
+def networkDataNodesAndEdges(dataset):
+    sourceNodes = list(dataset['Source'].values)
+    targetNodes = list(dataset['Target'].values)
+    nodes = list(set(sourceNodes).union(set(targetNodes)))
+
+    edges = list(set(zip(sourceNodes, targetNodes)))
+
+    return nodes, edges
+
+'''
+Network for Year 2000
+'''
+# zero_nodes, zero_edges = networkDataNodesAndEdges(zero_ds)
+#
+# G = nx.DiGraph()
+# G.add_nodes_from(zero_nodes)
+# G.add_edges_from(zero_edges)
+# # nx.draw(G,pos=nx.spring_layout(G))
+# # plt.draw()
+# # plt.show()
+#
+# # ASSORTATIVITY COEFFICIENT
+# r_orig = nx.degree_assortativity_coefficient(G)
+# r_form = nx.degree_assortativity_coefficient(G,x='out', y='out')
+#
+# print(r_orig, r_form)
+
+'''
+Network for Year 2010
+'''
+# ten_nodes, ten_edges = networkDataNodesAndEdges(ten_ds)
+#
+# G = nx.DiGraph()
+# G.add_nodes_from(ten_nodes)
+# G.add_edges_from(ten_edges)
+# # nx.draw(G,pos=nx.spring_layout(G))
+# # plt.draw()
+# # plt.show()
+#
+# # ASSORTATIVITY COEFFICIENT
+# r_orig = nx.degree_assortativity_coefficient(G)
+# r_form = nx.degree_assortativity_coefficient(G,x='out', y='out')
+#
+# print(r_orig, r_form)
+
+'''
+Network for Year 2017
+'''
+seventeen_nodes, seventeen_edges = networkDataNodesAndEdges(seventeen_ds)
+
+G = nx.DiGraph()
+G.add_nodes_from(seventeen_nodes)
+G.add_edges_from(seventeen_edges)
+# nx.draw(G,pos=nx.spring_layout(G))
+# plt.draw()
+# plt.show()
+
+# ASSORTATIVITY COEFFICIENT
+r_orig = nx.degree_assortativity_coefficient(G)
+r_form = nx.degree_assortativity_coefficient(G,x='out', y='out')
+
+print(r_orig, r_form)
--- playground.py
+++ playground.py
@@ -1,45 +1,102 @@
 import pandas as pd
 
 
-countries = pd.read_csv('ten_year_window_data/countries.csv', sep=',')
+# countries = pd.read_csv('ten_year_window_data_raw/countries.csv', sep=',')
 
-sipriDF = pd.read_csv('ten_year_window_data/sipri-arms-by-seller-1950.csv', sep=',')
+year = "2017"
+sipriDF = pd.read_csv('data/{}.csv'.format(year), sep=',')
 
-longSource = []
-latSource = []
-longDest = []
-latDest = []
+# print(sipriDF['buyer'].value_counts())
+# print(sipriDF['seller'].value_counts())
 
-for idx, rowVal in sipriDF.iterrows():
-    buyer = rowVal['buyer']
-    seller = rowVal['seller']
-    tempLongSource = -12.843791
-    tempLatSource = -24.545272
-    tempLongDest = 65.865034
-    tempLatDest = -25.559085
+# longSource = []
+# latSource = []
+# longDest = []
+# latDest = []
+#
+# for idx, rowVal in sipriDF.iterrows():
+#     buyer = rowVal['buyer']
+#     seller = rowVal['seller']
+#     tempLongSource = -12.843791
+#     tempLatSource = -24.545272
+#     tempLongDest = 65.865034
+#     tempLatDest = -25.559085
+#
+#     countriesRow = countries.loc[countries['name'] == seller]
+#
+#     if not countriesRow.empty:
+#         tempLongSource = countriesRow['longitude'].values[0]
+#         tempLatSource = countriesRow['latitude'].values[0]
+#
+#     countriesRow = countries.loc[countries['name'] == buyer]
+#
+#     if not countriesRow.empty:
+#         tempLongDest = countriesRow['longitude'].values[0]
+#         tempLatDest = countriesRow['latitude'].values[0]
+#
+#     longSource.append(tempLongSource)
+#     latSource.append(tempLatSource)
+#
+#     longDest.append(tempLongDest)
+#     latDest.append(tempLatDest)
+#
+#
+# sipriDF['longSource'] = pd.Series(longSource)
+# sipriDF['latSource'] = pd.Series(latSource)
+# sipriDF['longDest'] = pd.Series(longDest)
+# sipriDF['latDest'] = pd.Series(latDest)
+#
+# sipriDF.to_csv('{}.csv'.format(year))
 
-    countriesRow = countries.loc[countries['name'] == seller]
 
-    if not countriesRow.empty:
-        tempLongSource = countriesRow['longitude'].values[0]
-        tempLatSource = countriesRow['latitude'].values[0]
+'''
+Preprocessing nodes
+'''
+# nodesDF = pd.DataFrame()
+#
+# sourceNodes = set(list(sipriDF['seller'].values))
+# targetNodes = set(list(sipriDF['buyer'].values))
+#
+# tempNodes = list(sourceNodes.union(targetNodes))
+# nodes = pd.Series(tempNodes)
+# id = pd.Series(list(range(1, len(tempNodes)+1)))
+#
+# nodesDF['id'] = id
+# nodesDF['label'] = nodes
+#
+# nodesDF.to_csv('{}_processed_nodes.csv'.format(year), index=False)
 
-    countriesRow = countries.loc[countries['name'] == buyer]
 
-    if not countriesRow.empty:
-        tempLongDest = countriesRow['longitude'].values[0]
-        tempLatDest = countriesRow['latitude'].values[0]
+'''
+Preprocessing edges
+'''
 
-    longSource.append(tempLongSource)
-    latSource.append(tempLatSource)
+# edgesDF = sipriDF
+#
+# sourceNodeList = []
+# targetNodeList = []
+#
+# for idx, rowVal in sipriDF.iterrows():
+#     searchRow = nodesDF.loc[nodesDF['label'] == rowVal['seller']]
+#     sourceID = list(searchRow['id'])[0]
+#     sourceNodeList.append(sourceID)
+#
+#     searchRow = nodesDF.loc[nodesDF['label'] == rowVal['buyer']]
+#     targetID = list(searchRow['id'])[0]
+#     targetNodeList.append(targetID)
+#
+# # edgesDF['Source'] = pd.Series(sourceNodeList)
+# # edgesDF['Target'] = pd.Series(targetNodeList)
+#
+# edgesDF.insert(loc=0, column='Source', value=pd.Series(sourceNodeList))
+# edgesDF.insert(loc=1, column='Target', value=pd.Series(targetNodeList))
+#
+# edgesDF.to_csv('{}_edges.csv'.format(year), index=False)
 
-    longDest.append(tempLongDest)
-    latDest.append(tempLatDest)
+sipriDF = pd.read_csv('edges/{}_edges.csv'.format(year), sep=',')
 
+source = list(sipriDF['seller'].values)
+buyer = list(sipriDF['buyer'].values)
 
-sipriDF['longSource'] = pd.Series(longSource)
-sipriDF['latSource'] = pd.Series(latSource)
-sipriDF['longDest'] = pd.Series(longDest)
-sipriDF['latDest'] = pd.Series(latDest)
-
-sipriDF.to_csv('1950.csv')
+res = list(set(zip(source, buyer)))
+print(len(res))
